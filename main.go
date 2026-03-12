287872dc639f1f5af243bd0f98d25e3fc3d415b4
DegsRed <evanbrokeit@gmail.com>
2026-03-12 10:44:40 -0400

    Modified metrics message

--- main.go
+++ main.go
@@ -22,8 +22,8 @@ func main() {
 	}
 	serveMux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	serveMux.HandleFunc("GET /api/healthz", readiness)
-	serveMux.HandleFunc("GET /api/metrics", cfg.requests)
-	serveMux.HandleFunc("POST /api/reset", cfg.reset)
+	serveMux.HandleFunc("GET /admin/metrics", cfg.requests)
+	serveMux.HandleFunc("POST /admin/reset", cfg.reset)
 	log.Fatal(server.ListenAndServe())
 }
 
@@ -41,8 +41,15 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) requests(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
