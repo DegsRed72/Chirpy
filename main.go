d6bc5a113300692c1b786f7e335345dff30d28c5
DegsRed <evanbrokeit@gmail.com>
2026-03-13 16:52:02 -0400

    Added validate Chirp function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -24,6 +25,7 @@ func main() {
 	serveMux.HandleFunc("GET /api/healthz", readiness)
 	serveMux.HandleFunc("GET /admin/metrics", cfg.requests)
 	serveMux.HandleFunc("POST /admin/reset", cfg.reset)
+	serveMux.HandleFunc("POST /api/validate_chirp", validateChirp)
 	log.Fatal(server.ListenAndServe())
 }
 
@@ -54,4 +56,40 @@ func (cfg *apiConfig) requests(w http.ResponseWriter, r *http.Request) {
 
 func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits = atomic.Int32{}
+}
+
+func validateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error decoding parameters: %s", err))
+		return
+	}
+	respBody := params.Body
+	if len(respBody) > 140 {
+		respondWithError(w, 400, "Chirp too long")
+		return
+	}
+	type validity struct {
+		Valid bool `json:"valid"`
+	}
+	respondWithJSON(w, 200, validity{Valid: true})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.WriteHeader(code)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error marshalling JSON: %s", err))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write([]byte(dat))
 }
